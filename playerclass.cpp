d327d29d7574be361790b59742c6c2606d4621df
blackterrus <45721897+blackterrus@users.noreply.github.com>
2020-04-20 16:29:49 -0500

    Add comments and critiques to player class stub

--- playerclass.cpp
+++ playerclass.cpp
@@ -1,3 +1,17 @@
+/*
+ * Let's take a look at what's going on here:
+ * 1. Never import a full namespace. Import only what you need (e.g. I need cout, so I import std::cout) to avoid bloat
+ * 2. That entire class player{...} declaration should be in a separate header file
+ * 3. int player_hand[] shouldn't compile. Arrays without size are supposed to be impossible. Remember, C++ is a compiled language.
+ * 4. I'm not a fan of multiple declarations on one line. It's cleaner to do it on separate lines.
+ * 5. The players hand is a little more nuanced than a simple array.
+ *	- An integer only allows for consideration of value, and in the game suit is also important
+ *      - A struct with fields for value and suit is a better solution
+ *
+ * VERDICT: We need to work on the python formatting, but for the most part your class is conceptually pretty accurate
+ *	    in terms of understanding (i.e. public member functions, private member fields, etc.)
+ */
+
 using namespace std;
 
 class player{
