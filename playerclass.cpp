9c48a1b6b2b2daddb241246f93b791568c7b6a4a
andaehh <55859630+andaehh@users.noreply.github.com>
2020-04-20 21:51:14 -0500

    New version of the file
    I know it's not correct, but you wanted to check it before I put it into the actual .h

--- playerclass.cpp
+++ playerclass.cpp
@@ -1,29 +1,50 @@
-/*
- * Let's take a look at what's going on here:
- * 1. Never import a full namespace. Import only what you need (e.g. I need cout, so I import std::cout) to avoid bloat
- * 2. That entire class player{...} declaration should be in a separate header file
- * 3. int player_hand[] shouldn't compile. Arrays without size are supposed to be impossible. Remember, C++ is a compiled language.
- * 4. I'm not a fan of multiple declarations on one line. It's cleaner to do it on separate lines.
- * 5. The players hand is a little more nuanced than a simple array.
- *	- An integer only allows for consideration of value, and in the game suit is also important
- *      - A struct with fields for value and suit is a better solution
- *
- * VERDICT: We need to work on the python formatting, but for the most part your class is conceptually pretty accurate
- *	    in terms of understanding (i.e. public member functions, private member fields, etc.)
- */
+/* I dont have my compiler set up for this so I can't compile.
+*  I know that this is probably is the incorrect way to do this, but It makes sense to me
+*  Thehe getter is what is confusing ,e the most. I can't figure out how to pass the pointer from the setter to this function to set the player_hand from card_transfer
+*  I am using a for loop to set the first 15 cards (0-14) to the card_transer cariable
+*/
 
-using namespace std;
+
+
+#ifndef PLAYERCLASS_CPP_
+#define PLAYERCLASS_CPP_
+#include "../include/Deck.h"
+#include <vector>
 
 class player{
-	private:
-		int player_hand[], player_score;//int array for the player's hand. thinking of doing numerals for the cards ex. 1209 is a red, hearts, 9.
-	public:
-		void playerDraw(int), playerPlace(); //playerDraw() - card is added to player_hand[]. playerPlace() - card is removed from hand and placed on table.
-		player(); //constructor needs to build the player's hand
-		~player(); //destructor will erase all data held in class player
-}pc;
-
-player::player(){
-	//setHand()	- calls player_hand[] and sets the array with 15 cards (0-14)
-}
+  private:
+    typedef std::vector<class player> player_hand;
+	int player_score;
 	
+  public:
+  
+    //getter: used to place the data where it needs to go.
+	const toHand(const card_transfer)
+	{
+	  player_hand = card_transfer;
+	};
+	
+	//setter: used to initialize a vector or other data structure with data
+	void fromDeck(std::vector<transfer*> const &card_transfer)
+	{
+	  for (int i=0; i<=14; i++) 
+	  {    
+        card_transfer.push_back(deck_[i]) 
+	  };
+	
+	
+	player(); //constructor needs to build the player's hand
+	~player(); //destructor will erase all data held in class player
+		
+}pc; 
+
+#endif
+
+
+//getters and setters from the deck to the hand (encapsulation: preventing private members from being changed unexpectedly)
+//vectors for the player_hand
+//for (int i=0; i<14; i++) 
+//  player_hand.push_back(deck_[i]); 
+
+
+
